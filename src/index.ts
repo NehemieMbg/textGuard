e02578162c93e758bb9e44034f3042ad3e34be8b
NehemieMbg <nehemie.mbg@gmail.com>
2023-08-29 07:40:58 +0200

    Changed access

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
+/**
+ * Adds two number together
+ * @param a number input
+ * @param b number input
+ * @returns the computation of the twon inputs number
+ */
 export const add = (a: number, b: number) => {
   return a + b;
 };
